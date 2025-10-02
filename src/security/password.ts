6c6c1c11b332801383ded370e689808018a116ac
Marvelous Ukuesa <112257437+MarvelousUkuesa@users.noreply.github.com>
2025-10-02 17:39:34 +0200

    Update password.ts

--- src/security/password.ts
+++ src/security/password.ts
@@ -1,4 +1,4 @@
-export const PASSWORD_COST = 12; // recommended minimum
+export const PASSWORD_COST = 15; // recommended minimum
 
 export function getPasswordCost(): number {
   return PASSWORD_COST;
